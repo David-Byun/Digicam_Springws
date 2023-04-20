dc575d7ad51f7175f75d7b14e633faefbb96a039
David Byun <byundavid@naver.com>
2023-04-20 17:18:45 +0900

    0420 과제

--- clone0420/src/main/java/com/kbstar/controller/MainController.java
+++ clone0420/src/main/java/com/kbstar/controller/MainController.java
@@ -1,13 +1,32 @@
 package com.kbstar.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        return "index";
+    }
+
+    @RequestMapping("/card")
+    public String card(Model model) {
+        model.addAttribute("page", "card");
+        return "index";
+    }
+
+    @RequestMapping("/chart")
+    public String chart(Model model) {
+        model.addAttribute("page", "chart");
+        return "index";
+    }
+
+    @RequestMapping("/table")
+    public String table(Model model) {
+        model.addAttribute("page", "table");
         return "index";
     }
 
